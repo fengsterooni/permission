cd1fa2e365f39d43a71979160967f68e1aeaf10f
Feng Guo <fengsterooni@yahoo.com>
2020-06-15 18:47:26 -0700

    Quick reformat code.

--- app/src/main/java/com/example/permissions/MainActivity.kt
+++ app/src/main/java/com/example/permissions/MainActivity.kt
@@ -1,14 +1,12 @@
 package com.example.permissions
 
-import android.content.Context
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.jar.Manifest
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,17 +14,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnCameraPermission.setOnClickListener {
-            if (ContextCompat.checkSelfPermission(this,
-                            android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
-                    ContextCompat.checkSelfPermission(this,
-                            android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
             ) {
-                Toast.makeText(this, "You already have the permission for camera and GPS", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    this,
+                    "You already have the permission for camera and GPS",
+                    Toast.LENGTH_LONG
+                ).show()
             } else {
-                ActivityCompat.requestPermissions(this,
-                        arrayOf(
-                                android.Manifest.permission.CAMERA,
-                                android.Manifest.permission.ACCESS_FINE_LOCATION
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        android.Manifest.permission.CAMERA,
+                        android.Manifest.permission.ACCESS_FINE_LOCATION
                         ), CAMERA_AND_FINE_LOCATION_PERMISSION_CODE)
             }
         }
@@ -37,10 +44,17 @@ class MainActivity : AppCompatActivity() {
 
         if (requestCode == CAMERA_PERMISSSION_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(this, "Permission granted for camera", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    this,
+                    "Permission granted for camera",
+                    Toast.LENGTH_LONG
+                ).show()
             } else {
-                Toast.makeText(this,
-                        "Oops, NO PERMISSION", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    this,
+                    "Oops, NO PERMISSION",
+                    Toast.LENGTH_LONG
+                ).show()
             }
         }
     }
